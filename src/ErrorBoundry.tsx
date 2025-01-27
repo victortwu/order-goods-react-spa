9a831f65c0c35e1a1ba13f5ea333e08299515105
victor-twu <victor.twu@slalom.com>
2025-01-27 13:06:44 -0800

    added error boundry

---
+++ src/ErrorBoundry.tsx
@@ -0,0 +1,41 @@
+import { Component, ReactNode } from "react";
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+  error?: Error | null;
+}
+
+export class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error("Error Boundary caught an error:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{ padding: "20px", border: "1px solid red", color: "red" }}>
+          <h2>Something went wrong.</h2>
+          <p>{this.state.error?.message}</p>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import awsconfig from "./aws-exports.ts";
 import { BrowserRouter } from "react-router";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { App } from "./App.tsx";
+import { ErrorBoundary } from "./ErrorBoundry.tsx";
 import { ThemeProvider } from "./contexts/ThemeProvider/ThemeProvider.tsx";
 import { UserProvider } from "./contexts/UserContext/UserProvider.tsx";
 import "@aws-amplify/ui-react/styles.css";
@@ -16,14 +17,16 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <UserProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </UserProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <UserProvider>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </UserProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </ErrorBoundary>
   </StrictMode>
 );
